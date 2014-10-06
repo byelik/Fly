6f48eb9f71c4e8fc335be40df09b2e96f0314009
Vladimir Bilichenko <belikwolf@gmail.com>
2014-10-06 20:16:04 +0300

    ItemScene

---
+++ Fly/Assets/Scripts/Item.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Item.
+/// </summary>
+public class Item : MonoBehaviour
+{
+	[SerializeField]
+	private string itemName;
+	/// <summary>
+	/// Selects the item.
+	/// </summary>
+	public void selectItem()
+	{
+		//DO SOMETHING...
+		PlayerPrefs.SetString("ItemName", itemName);
+	}
+}
